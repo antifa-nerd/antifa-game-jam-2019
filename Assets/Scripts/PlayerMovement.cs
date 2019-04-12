38450e36650ebbac4d8abf51819d786c3948d3a8
Lorenzo Massidda <lorenzomassidda@hotmail.com>
2019-04-12 21:24:55 +0200

    Movimento Giocatore

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        transform.position += movement * Time.deltaTime * speed;
+    }
+
+}
