91f16ca9a5583cdab77d7d5611c0060f50f9016d
Mattia Belletti <mattia.belletti@gmail.com>
2019-04-12 22:15:08 +0200

    Basic enemy collision.

---
+++ Assets/Scripts/EnemySight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            System.Diagnostics.Debug.WriteLine("Collision!");
+        }
+    }
+}
