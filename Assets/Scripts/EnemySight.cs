138fd6c879c83a0392d651b3f1574efb59cf8aa6
Mattia Belletti <mattia.belletti@gmail.com>
2019-04-12 23:10:07 +0200

    Activating "!!!" on player detection

--- Assets/Scripts/EnemySight.cs
+++ Assets/Scripts/EnemySight.cs
@@ -4,18 +4,49 @@ using UnityEngine;
 
 public class EnemySight : MonoBehaviour
 {
+    public bool Alerted = false;
+
+    private Coroutine ResettingAlerted = null;
+
+    public float ResettingTime = 3;
+
+    public GameObject Exclamation;
+
     // Start is called before the first frame update
     void Start()
     {
         UnityEngine.Debug.Log("Start!");
+        Exclamation.SetActive(false);
     }
 
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // if (other.gameObject.tag == "Player")
-        // {
-        UnityEngine.Debug.Log("Collision!");
-        // }
+        if (other.gameObject.tag == "Player")
+        {
+            if (ResettingAlerted != null)
+            {
+                StopCoroutine(ResettingAlerted);
+                ResettingAlerted = null;
+            }
+            Alerted = true;
+            Exclamation.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            ResettingAlerted = StartCoroutine(ResetAlerted());
+        }
+    }
+
+    private IEnumerator ResetAlerted()
+    {
+        yield return new WaitForSeconds(3);
+        Alerted = false;
+        Exclamation.SetActive(false);
+        ResettingAlerted = null;
     }
 }
