c751f9c4feb3630adae5475b74359ddec0182270
Mattia Belletti <mattia.belletti@gmail.com>
2019-04-12 22:26:00 +0200

    Enemy collision.

--- Assets/Scripts/EnemySight.cs
+++ Assets/Scripts/EnemySight.cs
@@ -7,15 +7,15 @@ public class EnemySight : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        UnityEngine.Debug.Log("Start!");
     }
 
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Player")
-        {
-            System.Diagnostics.Debug.WriteLine("Collision!");
-        }
+        // if (other.gameObject.tag == "Player")
+        // {
+        UnityEngine.Debug.Log("Collision!");
+        // }
     }
 }
