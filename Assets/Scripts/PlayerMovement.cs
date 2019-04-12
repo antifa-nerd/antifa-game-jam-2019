5ea9bb5147ea42bf00aef517ca574791a42032ed
Lorenzo Massidda <lorenzomassidda@hotmail.com>
2019-04-12 21:59:34 +0200

    Player movement discretizzato

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,36 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 10f;
+    public float stepTimeDirect = 0.15f;
+    public float stepTimeDiagonal = 0.20f;
 
+    float lastUpdate = 0.15f;
     // Update is called once per frame
     void Update()
     {
-        Vector3 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Vector3 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            if (movement.x!=0)
+                movement.x=Mathf.Sign(movement.x);
+            if (movement.y!=0)
+                movement.y=Mathf.Sign(movement.y);
 
-        transform.position += movement * Time.deltaTime * speed;
+        if (Mathf.Abs(movement.x)==1 && Mathf.Abs(movement.y)==1)
+        {
+            if (lastUpdate > stepTimeDiagonal){
+                transform.position+=movement;
+                lastUpdate = 0f;
+            }
+        }
+        else
+        {
+            if (lastUpdate > stepTimeDirect){
+                transform.position+=movement;
+                lastUpdate = 0f;
+            }
+        }
+
+
+        lastUpdate+=Time.deltaTime;
     }
 
 }
