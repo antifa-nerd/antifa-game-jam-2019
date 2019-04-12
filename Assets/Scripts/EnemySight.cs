6b9edcc0b8895e64a8b92cc65ff93db440ee5529
Mattia Belletti <mattia.belletti@gmail.com>
2019-04-12 22:15:58 +0200

    Remove useless method.

--- Assets/Scripts/EnemySight.cs
+++ Assets/Scripts/EnemySight.cs
@@ -11,11 +11,6 @@ public class EnemySight : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
