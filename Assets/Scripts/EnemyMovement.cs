5b83852aee977c33b4be0499a1eddacbf8055159
Mattia Belletti <mattia.belletti@gmail.com>
2019-04-13 17:27:12 +0200

    Basic pathfinding

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -9,6 +9,8 @@ public class EnemyMovement : MonoBehaviour
 
     private int currentStep = 0;
 
+    private int currentWaypoint = 0;
+
     public float Speed = 10f;
 
     public float RotationSpeed = 1f;
@@ -42,21 +44,32 @@ public class EnemyMovement : MonoBehaviour
     {
         Path = p;
         ComputingPath = false;
+        currentWaypoint = 0;
         Debug.Log("Yay, we got a path back. Did it have an error? " + p.error);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // get the current path we're following
+        // compute start and end point of this patrolling area
         var startPosition = transform.position;
-        var endPosition = Steps[currentStep].transform.position;
+        var patrollingEndPosition = Steps[currentStep].transform.position;
 
+        // if we need to compute to path: run it
         if (Path == null && !ComputingPath)
         {
             ComputingPath = true;
-            Seeker.StartPath(startPosition, endPosition);
+            Seeker.StartPath(startPosition, patrollingEndPosition);
         }
+
+        // if we still don't have the path: don't move yet
+        if (Path == null)
+        {
+            return;
+        }
+
+        // get the end position for this waypoint
+        var endPosition = Path.vectorPath[currentWaypoint];
 
         // move along the path
         var delta = Speed * Time.deltaTime;
@@ -66,15 +79,24 @@ public class EnemyMovement : MonoBehaviour
         // move to the next path part if completed it
         if (delta > totalDistance)
         {
-            Path = null;
-            currentStep = (currentStep + 1) % Steps.Length;
+            currentWaypoint++;
+            if (currentWaypoint >= Path.vectorPath.Count)
+            {
+                Path = null;
+                currentStep = (currentStep + 1) % Steps.Length;
+            }
         }
 
         // rotate according to the direction
-        var destUp = endPosition - transform.position;
-        if (transform.up != destUp)
+        var destUp = (endPosition - transform.position).normalized;
+        if ((transform.up - destUp).sqrMagnitude > 0.1f)
         {
-            transform.up = Vector3.Lerp(transform.up, destUp, RotationSpeed / (destUp - transform.up).magnitude);
+            var newUp = Vector3.Lerp(transform.up, destUp, RotationSpeed * Time.deltaTime / (destUp - transform.up).magnitude);
+            if (transform.up != newUp)
+            {
+                Debug.Log(string.Format("{2}: Changing up from {0} to {1}", transform.up, newUp, Time.time));
+            }
+            transform.up = newUp;
         }
     }
 }
