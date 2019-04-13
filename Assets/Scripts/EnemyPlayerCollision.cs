961eaa9b3bc31ad043ebdeb6e181a30933966195
Mattia Belletti <mattia.belletti@gmail.com>
2019-04-13 21:54:45 +0200

    Added a Game Over screen upon enemy/player collision.

---
+++ Assets/Scripts/EnemyPlayerCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyPlayerCollision : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene("GameOver", LoadSceneMode.Single);
+        }
+    }
+}
