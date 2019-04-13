d8cc7411df7b766e56e8588731f6055b31ddf1fb
Mattia Belletti <mattia.belletti@gmail.com>
2019-04-13 22:01:16 +0200

    Added radius to enemy influence

--- Assets/Scripts/EnemyPlayerCollision.cs
+++ Assets/Scripts/EnemyPlayerCollision.cs
@@ -5,6 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class EnemyPlayerCollision : MonoBehaviour
 {
+    public float Radius = 0.5f;
+
+    private void Start()
+    {
+        GetComponent<CircleCollider2D>().radius = Radius;
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
